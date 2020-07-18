f0e5eccec2f3c0c3c3b99430d86a6b04c0d0ec36
Badri <programmingwithbadri@outlook.com>
2020-07-18 23:21:46 +0530

    Serialize the custom errors and convert it to response objects

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 export const errorHandler = (
   err: Error,
@@ -6,6 +8,17 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
+  if (err instanceof RequestValidationError) {
+    return res.status(err.statusCode).send({
+      errors: err.serializeErrors()
+    })
+  }
+
+  if (err instanceof DatabaseConnectionError) {
+    return res.status(err.statusCode).send({
+      errors: err.serializeErrors()
+    })
+  }
   res.status(400).send({
     message: err.message
   });
