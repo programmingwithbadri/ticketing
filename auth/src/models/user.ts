3c2f70594ca64c0321a6555bae8a05d14d2c5005
Badri <programmingwithbadri@outlook.com>
2020-07-19 20:13:17 +0530

    Hash the password before storing it in db

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,37 +1,50 @@
 import mongoose from 'mongoose';
+import { Password } from '../utils/password'
 
 // An interface that describes the properties
 // that are required to create a new User
 interface UserAttrs {
-  email: string;
-  password: string;
+    email: string;
+    password: string;
 }
 
 // An interface that describes the properties
 // that a User Model has
 interface UserModel extends mongoose.Model<UserDoc> {
-  build(attrs: UserAttrs): UserDoc;
+    build(attrs: UserAttrs): UserDoc;
 }
 
 // An interface that describes the properties
 // that a User Document has
 interface UserDoc extends mongoose.Document {
-  email: string;
-  password: string;
+    email: string;
+    password: string;
 }
 
 const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
 });
+
+// This will be called everytime before the mongoose save method
+userSchema.pre('save', async function (done) {
+    // If the save operation involves the pwd field of document
+    if (this.isModified('password')) {
+        const hashedPwd = await Password.toHash(this.get('password'));
+        this.set('password', hashedPwd);
+    }
+
+    done(); // done is inbuilt mongoose method to ack, operation is done
+})
+
 userSchema.statics.build = (attrs: UserAttrs) => {
-  return new User(attrs);
+    return new User(attrs);
 };
 
 // User of type User model
