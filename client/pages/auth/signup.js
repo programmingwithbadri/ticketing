f289a3a1d20498a7b16494c777d1f295b6ac2243
Badri <programmingwithbadri@outlook.com>
2020-07-23 17:00:35 +0530

    Handle email and password and signup with auth service

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,14 +1,37 @@
+import { useState } from "react";
+import axios from "axios";
+
 export default () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    const response = await axios.post("/api/users/signup", { email, password });
+
+    console.log(response);
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h1>Sign Up</h1>
       <div className="form-group">
         <label>Email Address</label>
-        <input className="form-control" />
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="form-control"
+        />
       </div>
       <div className="form-group">
         <label>Password</label>
-        <input type="password" className="form-control" />
+        <input
+          type="password"
+          className="form-control"
+          onChange={(e) => setPassword(e.target.value)}
+          className="form-control"
+        />
       </div>
       <button className="btn btn-primary">Sign Up</button>
     </form>
