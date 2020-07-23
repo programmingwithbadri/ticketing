6d3e3f05bf5396705080511e507cf0016fe05a81
Badri <programmingwithbadri@outlook.com>
2020-07-23 19:44:28 +0530

    Create the hook to handle the axios request in common place

--- client/next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    // Fix for file change detection in browser
-    webpackDevMiddleware: config => {
-      // Assigning that we need to pull all diff files for every 300ms
-      config.watchOptions.poll = 300;
-      return config;
-    }
-  };
-  
