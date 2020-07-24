1bcc43754b198832f39c3de64ddc8f05cff0c956
Badri <programmingwithbadri@outlook.com>
2020-07-24 12:31:05 +0530

    Handle the props in app component and add the header component

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,12 +1,32 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
+import Header from '../components/header';
 
-// Wrapper for all the components of the next js components(pages)
-const AppComponent = ({ Component, pageProps }) => {
+const AppComponent = ({ Component, pageProps, currentUser }) => {
   return (
     <div>
+      <Header currentUser={currentUser} />
       <Component {...pageProps} />
     </div>
   );
+};
+
+AppComponent.getInitialProps = async appContext => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+
+  let pageProps = {};
+
+  // Checks if the child component have any getInitialProps defined
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  // Pass the props and currentUserData to child component
+  return {
+    pageProps,
+    ...data
+  };
 };
 
 export default AppComponent;
