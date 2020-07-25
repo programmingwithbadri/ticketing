94577f1047c797cfe1fc3b55771d289c0bfea110
Badri <programmingwithbadri@outlook.com>
2020-07-25 11:40:40 +0530

    Remove the errors and middlewares from auth service and instead use the common library

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -2,13 +2,12 @@ import express from 'express';
 import 'express-async-errors'; // handles the promise of async method if we throw any error in it
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError } from '@dev-ticketing/common';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.set('trust proxy', true); // Setting true so that it will trust req from k8s services
