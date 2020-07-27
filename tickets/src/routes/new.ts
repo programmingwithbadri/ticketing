202d8846ee19e9b272e62d7db06fa5335d7d0066
Badri <programmingwithbadri@outlook.com>
2020-07-27 17:28:07 +0530

    Validating price and title in tickets service TDD

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,11 +1,22 @@
 import express, { Request, Response } from 'express';
-import { requireAuth } from '@dev-ticketing/common';
+import { body } from 'express-validator';
+import { requireAuth, validateRequest } from '@dev-ticketing/common';
 
 const router = express.Router();
 
 router.post(
     '/api/tickets',
     requireAuth,
+    [
+        body('title')
+            .notEmpty()
+            .withMessage('Title is required'),
+        body('price')
+            .isFloat({ gt: 0 }) // Greater than zero 
+            .notEmpty()
+            .withMessage('Price must be greater than zero')
+    ],
+    validateRequest,
     async (req: Request, res: Response) => {
 
         res.sendStatus(200);
