da87424bfd42f679161902a7e1b5ab51a2645723
Badri <programmingwithbadri@outlook.com>
2020-07-27 17:12:21 +0530

    Add the require auth middleware to auth users - TDD

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response } from 'express';
+import { requireAuth } from '@dev-ticketing/common';
 
 const router = express.Router();
 
 router.post(
     '/api/tickets',
+    requireAuth,
     async (req: Request, res: Response) => {
 
         res.sendStatus(200);
