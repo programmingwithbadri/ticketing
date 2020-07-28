8d2284a7818c24968c7019ea381624657ce260e4
Badri <programmingwithbadri@outlook.com>
2020-07-28 23:27:19 +0530

    Listening to the events (Nats-test)

---
+++ nats-test/src/listener.ts
@@ -0,0 +1,17 @@
+import nats, { Message } from 'node-nats-streaming';
+
+console.clear();
+
+const stan = nats.connect('ticketing', '123', {
+    url: 'http://localhost:4222',
+});
+
+stan.on('connect', () => {
+    console.log('Listener connected to NATS');
+
+    const subscription = stan.subscribe('ticket:created');
+
+    subscription.on('message', (msg: Message) => {
+        console.log('Message recieved')
+    });
+});
