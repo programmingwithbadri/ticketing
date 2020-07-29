7302a8c2c5fa0cb448a0bf038695155576a9f384
Badri <programmingwithbadri@outlook.com>
2020-07-29 21:19:18 +0530

    Create the custom publisher base class and refactor existing one - nats-test

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,4 +1,5 @@
 import nats from 'node-nats-streaming';
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
 
 console.clear();
 
@@ -8,17 +9,17 @@ const stan = nats.connect('ticketing', 'abc', {
 });
 
 // Listen on connect event
-stan.on('connect', () => {
+stan.on('connect', async () => {
   console.log('Publisher connected to NATS');
 
-  const data = JSON.stringify({
-    id: '123',
-    title: 'concert',
-    price: 20,
-  });
-
-  // Publish an event to the nats streaming server
-  stan.publish('ticket:created', data, () => {
-    console.log('Event published');
-  });
+  const publisher = new TicketCreatedPublisher(stan);
+  try {
+    await publisher.publish({
+      id: '123',
+      title: 'concert',
+      price: 20,
+    });
+  } catch (err) {
+    console.error(err);
+  }
 });
