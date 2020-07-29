60543aad7ef19b1a3a093dd7f5598121d4c9d766
Badri <programmingwithbadri@outlook.com>
2020-07-29 20:46:36 +0530

    Update the base listener as generic and leverage the typescript type checking - nats-test

--- nats-test/src/events/base-listener.ts
+++ nats-test/src/events/base-listener.ts
@@ -1,9 +1,15 @@
 import { Message, Stan } from 'node-nats-streaming';
+import { Subjects } from './subjects';
 
-export abstract class Listener {
-    abstract subject: string;
+interface Event {
+    subject: Subjects;
+    data: any;
+}
+
+export abstract class Listener<T extends Event> {
+    abstract subject: T['subject'];
     abstract queueGroupName: string;
-    abstract onMessage(data: any, msg: Message): void;
+    abstract onMessage(data: T['data'], msg: Message): void;
     private client: Stan;
     protected ackWait = 5 * 1000;
 
@@ -38,7 +44,7 @@ export abstract class Listener {
     parseMessage(msg: Message) {
         const data = msg.getData();
         return typeof data === 'string'
-            ? JSON.parse(data) // string to json
-            : JSON.parse(data.toString('utf8')); // buffer type to json
+            ? JSON.parse(data)
+            : JSON.parse(data.toString('utf8'));
     }
 }
