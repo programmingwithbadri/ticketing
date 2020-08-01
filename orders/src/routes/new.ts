419a1091885a8d8003097ea05df3c368c5595564
Badri <programmingwithbadri@outlook.com>
2020-08-01 12:52:28 +0530

    Associate the ticket and order model using mongoose ref in orders service

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,10 +1,20 @@
+import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
-import { requireAuth } from '@dev-ticketing/common';
+import { requireAuth, validateRequest } from '@dev-ticketing/common';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/orders', requireAuth, async (req: Request, res: Response) => {
-    res.send({});
-});
+router.post('/api/orders', requireAuth,
+    [
+        body('ticketId')
+            .not()
+            .isEmpty()
+            .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+            .withMessage('TicketId must be provided'),
+    ],
+    validateRequest, async (req: Request, res: Response) => {
+        res.send({});
+    });
 
 export { router as newOrderRouter };
