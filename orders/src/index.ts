bc7150e4414166585cade29ebc64ad09816f0e5f
Badri <programmingwithbadri@outlook.com>
2020-08-05 12:50:05 +0530

    Initialize the event listeners in orders service

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -2,6 +2,8 @@ import mongoose from 'mongoose';
 
 import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
+import { TicketCreatedListener } from './events/listeners/ticket-created-listener';
+import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -36,6 +38,10 @@ const start = async () => {
     // Close the connection when the terminal is closed/ Or connection issue
     process.on('SIGINT', () => natsWrapper.client.close()); // Interrupt connection
     process.on('SIGTERM', () => natsWrapper.client.close()); // Terminate connection
+
+    // Listen for the events
+    new TicketCreatedListener(natsWrapper.client);
+    new TicketUpdatedListener(natsWrapper.client);
 
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
