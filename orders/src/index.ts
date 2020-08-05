82abe1e8905f65339358d1a4eaf3bcd07cd3830d
Badri <programmingwithbadri@outlook.com>
2020-08-05 13:07:17 +0530

    Listen to the initialized listeners

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -40,8 +40,8 @@ const start = async () => {
     process.on('SIGTERM', () => natsWrapper.client.close()); // Terminate connection
 
     // Listen for the events
-    new TicketCreatedListener(natsWrapper.client);
-    new TicketUpdatedListener(natsWrapper.client);
+    new TicketCreatedListener(natsWrapper.client).listen();
+    new TicketUpdatedListener(natsWrapper.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
