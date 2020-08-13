3d9bc578d0731a1be248c704a789b0d97f54f31b
Badri <programmingwithbadri@outlook.com>
2020-08-13 17:10:51 +0530

    fix the failing tests in orders service

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 import { OrderStatus } from '@dev-ticketing/common';
 import { TicketDoc } from './ticket';
 
@@ -53,6 +54,10 @@ const orderSchema = new mongoose.Schema(
     },
   }
 );
+
+// Store __v(version key) value in the version field
+orderSchema.set('versionKey', 'version');
+orderSchema.plugin(updateIfCurrentPlugin);
 
 orderSchema.statics.build = (attrs: OrderAttrs) => {
   return new Order(attrs);
