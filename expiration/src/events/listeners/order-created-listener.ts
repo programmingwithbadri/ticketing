f65b36d196b900448fe2c26a6ebedf57f6bc3d39
Badri <programmingwithbadri@outlook.com>
2020-08-20 17:02:44 +0530

    Queueing the job when order is created

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,12 +1,25 @@
 import { Message } from 'node-nats-streaming';
 import { Listener, OrderCreatedEvent, Subjects } from '@dev-ticketing/common';
 import { queueGroupName } from '../constants';
+import { expirationQueue } from '../../queues/expiration-queue';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     subject: Subjects.OrderCreated = Subjects.OrderCreated;
     queueGroupName = queueGroupName;
 
     async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+        const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+        console.log('Waiting this many milliseconds to process the job:', delay);
 
+        await expirationQueue.add(
+            {
+                orderId: data.id,
+            },
+            {
+                delay, // How long to have the job inside the queue
+            }
+        );
+
+        msg.ack();
     }
 }
--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,4 +1,5 @@
 import { natsWrapper } from './nats-wrapper';
+import { OrderCreatedListener } from './events/listeners/order-created-listener';
 
 const start = async () => {
   if (!process.env.NATS_CLIENT_ID) {
@@ -27,6 +28,8 @@ const start = async () => {
     // Close the connection when the terminal is closed/ Or connection issue
     process.on('SIGINT', () => natsWrapper.client.close()); // Interrupt connection
     process.on('SIGTERM', () => natsWrapper.client.close()); // Terminate connection
+
+    new OrderCreatedListener(natsWrapper.client).listen();
   } catch (err) {
     console.error(err);
   }
