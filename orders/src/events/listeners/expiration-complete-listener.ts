97b89faa11f23a534977da8c3ae73cd8d678e5a4
Badri <programmingwithbadri@outlook.com>
2020-08-20 17:22:26 +0530

    Dont cancel the order if it is completed

--- orders/src/events/listeners/expiration-complete-listener.ts
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -22,6 +22,10 @@ export class ExpirationCompleteListener extends Listener<
             throw new Error('Order not found');
         }
 
+        if(order.status === OrderStatus.Complete) {
+            return msg.ack();
+        }
+
         // Cancel the order once the order expired
         order.set({
             status: OrderStatus.Cancelled,
